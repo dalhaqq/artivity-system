664357278050349dff7cf7d586d0fe145069b507
Narotama Basundara <narotamaaa3@gmail.com>
2024-06-05 12:21:55 +0700

    public store bukti pembayran

--- app/Http/Livewire/PrintPayment.php
+++ app/Http/Livewire/PrintPayment.php
@@ -29,6 +29,8 @@ class PrintPayment extends Component
         $extension = $this->filePayment->extension();
         $filePaymentName = "print_payment_" . Auth::user()->id . "_" . $uploadDate . '.' . $extension;
         $this->filePayment->storeAs("storage/payment_print_order", $filePaymentName);
+
+        Storage::disk('public')->putFileAs('payment_print_order', $this->filePayment, $filePaymentName);
         
         $filePrint = $this->printPayment['filepath'];
         $filePrintName = Auth::user()->id.'-'.Auth::user()->name.'-'.$uploadDate.'-'.'x'.$this->printPayment['jml_copy'].'-'.$this->printPayment['filename'];
